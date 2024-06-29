c61c4d553ed0e0699fe10750e451909488c0b4fe
IDS.ADE KENRIAN <ids.adek@xl.co.id>
2024-06-30 06:39:54 +0700

    update

--- src/main/java/org/demo/data/repository/jpa/DocumentJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package org.demo.data.repository.jpa;
-
-import org.demo.bean.jpa.DocumentEntity;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface DocumentJpaRepository extends PagingAndSortingRepository<DocumentEntity, Integer> {
-
-}
--- src/main/java/org/demo/data/repository/jpa/UserJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package org.demo.data.repository.jpa;
-
-import org.demo.bean.jpa.User;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface UserJpaRepository extends PagingAndSortingRepository<User, String> {
-    User findByUser(String username);
-}
