eb212aac1d4570497cd2108f64dfc118c0b3205b
IDS.ADE KENRIAN <ids.adek@xl.co.id>
2024-07-07 20:42:15 +0700

    fixing search box

--- src/main/java/com/cb/repository/UserRepository.java
+++ src/main/java/com/cb/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.cb.repository;
 
+import com.cb.model.Cabang;
 import com.cb.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+    Page<User> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
